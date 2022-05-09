5e1f31a3d3f717dc3b5ed9895793244fe5f8a435
yongun414 <melbong0069@gmail.com>
2022-05-09 20:57:22 +0900

    update

---
+++ 0411/1.py
@@ -0,0 +1,15 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/42626
+'''
+'''
+input
+info[0,1,0,1,1,0,1,0,0,1,0]	edges [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]	5
+'''
+from copy import deepcopy
+
+a = [1,2,3]
+b = a
+
+a.append(4)
+
+print(b)
