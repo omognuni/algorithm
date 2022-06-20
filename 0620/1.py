c9224d176974cffb92569ed3667c2f8c7f0d2d15
omognuni <melbong0069@gmail.com>
2022-06-20 23:16:31 +0900

    update

---
+++ 0620/1.py
@@ -0,0 +1,18 @@
+'''
+https://www.acmicpc.net/problem/10872
+'''
+import sys
+input = sys.stdin.readline
+
+def factorial(n):
+    if n == 0 :
+        return 0
+    if n == 1 :
+        return 1
+    else:
+        return n*factorial(n-1)
+
+
+n = int(input())
+
+print(factorial(n))
