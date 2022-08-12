554b29156fdeef0419a3568eacd15efa0efe3923
yongun414 <melbong0069@gmail.com>
2022-08-12 19:10:25 +0900

    update

--- 0809/15649.py
+++ 0809/15649.py
@@ -1,34 +1,52 @@
-# 순열
+
 import sys
 
 input = sys.stdin.readline
 
-
-def p(arr, k):
-
-    result = []
-
-    if k > len(arr) + 1:
-        return
-
-    if k == 1:
-        for i in arr:
-            result.append([i])
-
-    if k > 1:
-        for i in range(len(arr)):
-            for j in p(arr[:i] + arr[i+1:], k-1):
-
-                result.append([arr[i]] + j)
-
-    return result
-
+s = []
 
 n, m = map(int, input().split())
 
 
-arr = [str(i) for i in range(1, n+1)]
+# 백트래킹
+def dfs():
 
-ans = p(arr, m)
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
 
-[print(" ".join(i)) for i in ans]
+    for i in range(1, n+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+
+
+dfs()
+
+
+# 순열
+# def p(arr, k):
+
+#     result = []
+
+#     if k > len(arr) + 1:
+#         return
+
+#     if k == 1:
+#         for i in arr:
+#             result.append([i])
+
+#     if k > 1:
+#         for i in range(len(arr)):
+#             for j in p(arr[:i] + arr[i+1:], k-1):
+
+#                 result.append([arr[i]] + j)
+
+#     return result
+
+# arr = [str(i) for i in range(1, n+1)]
+
+# ans = p(arr, m)
+
+# [print(" ".join(i)) for i in ans]
--- 0809/15650.py
+++ 0809/15650.py
@@ -1,33 +1,50 @@
-# 조합
+
 import sys
 
 input = sys.stdin.readline
 
-
-def c(arr, k):
-
-    result = []
-
-    if k > len(arr) + 1:
-        return
-
-    if k == 1:
-        for i in arr:
-            result.append([i])
-
-    elif k > 1:
-        for i in range(len(arr) - k + 1):
-            for j in c(arr[i+1:], k-1):
-                result.append([arr[i]] + j)
-
-    return result
-
-
 n, m = map(int, input().split())
 
+# 백트래킹
+s = []
 
-arr = [str(i) for i in range(1, n+1)]
 
-ans = c(arr, m)
+def dfs(start):
 
-[print(" ".join(i)) for i in ans]
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
+
+    for i in range(start, n+1):
+        if i not in s:
+            s.append(i)
+            dfs(i+1)
+            s.pop()
+
+
+dfs(1)
+# 조합
+# def c(arr, k):
+
+#     result = []
+
+#     if k > len(arr) + 1:
+#         return
+
+#     if k == 1:
+#         for i in arr:
+#             result.append([i])
+
+#     elif k > 1:
+#         for i in range(len(arr) - k + 1):
+#             for j in c(arr[i+1:], k-1):
+#                 result.append([arr[i]] + j)
+
+#     return result
+
+
+# arr = [str(i) for i in range(1, n+1)]
+
+# ans = c(arr, m)
+
+# [print(" ".join(i)) for i in ans]
